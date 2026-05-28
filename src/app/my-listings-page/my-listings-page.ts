d52203105004b1f5be904a9e6e5af4130b69767c
Najibullah Mohammadi <mohammadi@MacBookPro.fritz.box>
2026-05-28 20:37:08 +0200

    generating my listings page

---
+++ src/app/my-listings-page/my-listings-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-listings-page',
+  imports: [],
+  templateUrl: './my-listings-page.html',
+  styleUrl: './my-listings-page.css',
+})
+export class MyListingsPage {}
