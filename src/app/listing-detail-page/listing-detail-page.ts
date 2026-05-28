cd79de8e953267e6f41e0fbcb3ee8e4d23ccfb76
Najibullah Mohammadi <mohammadi@MacBookPro.fritz.box>
2026-05-28 20:35:03 +0200

    generating listing detail page

---
+++ src/app/listing-detail-page/listing-detail-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-detail-page',
+  imports: [],
+  templateUrl: './listing-detail-page.html',
+  styleUrl: './listing-detail-page.css',
+})
+export class ListingDetailPage {}
