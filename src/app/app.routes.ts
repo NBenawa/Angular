74b5094d7663c068242c4b98fd079b4527222933
Najibullah Mohammadi <mohammadi@MacBookPro.fritz.box>
2026-05-28 20:44:44 +0200

    adding new pages to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
-import { ListingPage } from './listing-page/listing-page';
+import { ListingsPage } from './listings-page/listings-page';
+import { ContactPage } from './contact-page/contact-page';
+import { ListingDetailPage } from './listing-detail-page/listing-detail-page';
+import { EditListingPage } from './edit-listing-page/edit-listing-page';
+import { MyListingsPage } from './my-listings-page/my-listings-page';
+import { NewListingPage } from './new-listing-page/new-listing-page';
 
 export const routes: Routes = [
-    { path: 'listings', component: ListingPage, pathMatch: 'full' },
+    { path: '', redirectTo: 'listings', pathMatch: 'full' },
+    { path: 'listings', component: ListingsPage, pathMatch: 'full' },
+    { path: 'listings/:id', component: ListingDetailPage }, 
+    { path: 'contact/:id', component: ContactPage },
+    { path: 'edit-listing/:id', component: EditListingPage },
+    { path: 'my-listings', component: MyListingsPage },
+    { path: 'new-listing', component: NewListingPage }
 ];
--- src/app/listing-page/listing-page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-listing-page',
-  imports: [],
-  templateUrl: './listing-page.html',
-  styleUrl: './listing-page.css',
-})
-export class ListingPage {}
