b94422eede83358faa73b3db0f3b945c23e24e06
Najibullah Mohammadi <mohammadi@MacBookPro.fritz.box>
2026-05-28 20:37:41 +0200

    generating new listings page

---
+++ src/app/new-listing-page/new-listing-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-listing-page',
+  imports: [],
+  templateUrl: './new-listing-page.html',
+  styleUrl: './new-listing-page.css',
+})
+export class NewListingPage {}
