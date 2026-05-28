3f748b65f20e2a8dd01c33564fc988619532de5a
Najibullah Mohammadi <mohammadi@MacBookPro.fritz.box>
2026-05-28 20:36:10 +0200

    generating edit listing page

---
+++ src/app/edit-listing-page/edit-listing-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-listing-page',
+  imports: [],
+  templateUrl: './edit-listing-page.html',
+  styleUrl: './edit-listing-page.css',
+})
+export class EditListingPage {}
