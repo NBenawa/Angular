df11aed109eeb482627ef97e082d715052c438be
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-05-29 20:13:45 +0200

    updating new listing page accordingly

--- src/app/new-listing-page/new-listing-page.ts
+++ src/app/new-listing-page/new-listing-page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ListingDataForm } from '../listing-data-form/listing-data-form';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-listing-page',
@@ -8,4 +9,10 @@ import { ListingDataForm } from '../listing-data-form/listing-data-form';
   styleUrl: './new-listing-page.css',
 })
 export class NewListingPage {
+  constructor(private router: Router) {}
+
+  createListing() {
+    alert('Listing created!');
+    this.router.navigate(['/my-listings']);
+  }
 }
