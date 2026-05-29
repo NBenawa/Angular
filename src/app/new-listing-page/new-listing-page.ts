40549c5942a5f1458bcf696fe17e25ccf380d61a
Najibullah Mohammadi <mohammadi@MacBookPro.fritz.box>
2026-05-29 12:13:02 +0200

    completing new listing page

--- src/app/new-listing-page/new-listing-page.ts
+++ src/app/new-listing-page/new-listing-page.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-listing-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-listing-page.html',
   styleUrl: './new-listing-page.css',
 })
-export class NewListingPage {}
+export class NewListingPage {
+  name: string = '';
+  description: string = ''
+  price: string = '';
+
+  constructor(private router: Router) {}
+
+  createListing() {
+    alert('Listing created!'); // Placeholder for actual implementation
+    this.router.navigate(['/my-listings']);
+  }
+}
