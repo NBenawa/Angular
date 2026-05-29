470589fc4ff1e7d20b30192777fb74244a717191
Najibullah Mohammadi <mohammadi@MacBookPro.fritz.box>
2026-05-29 10:25:21 +0200

    completing listing detail page

--- src/app/listing-detail-page/listing-detail-page.ts
+++ src/app/listing-detail-page/listing-detail-page.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { fakeListings } from '../fake-data';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-detail-page',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './listing-detail-page.html',
   styleUrl: './listing-detail-page.css',
 })
-export class ListingDetailPage {}
+export class ListingDetailPage {
+  listing: Listing | undefined;
+
+  constructor(private route: ActivatedRoute) {    
+  }
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id);
+    }
+}
