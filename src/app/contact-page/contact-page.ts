b8881d89a198d15e8c08f69bc440c214d8421e23
Najibullah Mohammadi <mohammadi@MacBookPro.fritz.box>
2026-05-29 10:41:34 +0200

    completing contact page

--- src/app/contact-page/contact-page.ts
+++ src/app/contact-page/contact-page.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-page.html',
   styleUrl: './contact-page.css',
 })
-export class ContactPage {}
+export class ContactPage {
+  email: string = '';
+  message: string = '';
+  listing: Listing | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngonInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find((listing) => listing.id === id);
+    this.message = `Hi ${this.listing?.name}, I'm interested in your ` + this.listing?.name.toLowerCase() + `.`;
+  }
+
+  sendMessage() {
+    alert(`Message sent to the seller!`);
+    this.router.navigateByUrl('/listings');
+  }
+}
