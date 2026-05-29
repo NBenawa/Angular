09d2d28a7c262a0e79f6abb4962209bca8b5e5b5
Najibullah Mohammadi <mohammadi@MacBookPro.fritz.box>
2026-05-29 12:12:24 +0200

    completing my listings page

--- src/app/my-listings-page/my-listings-page.ts
+++ src/app/my-listings-page/my-listings-page.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { fakeMyListings } from '../fake-data';
+import { Listing } from '../types';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-listings-page',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './my-listings-page.html',
   styleUrl: './my-listings-page.css',
 })
-export class MyListingsPage {}
+export class MyListingsPage {
+  fakeMyListings: Listing[] = [];
+
+  constructor(
+  ){}
+
+  ngOnInit() {
+    this.fakeMyListings = fakeMyListings;
+  }
+
+  deleteListing(id: string) {
+    alert(`Delete listing with id: ${id}`);
+  }
+}
